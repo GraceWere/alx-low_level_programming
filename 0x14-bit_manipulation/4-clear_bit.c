8212f59de7a58ef445715b9126b63cf4f24f5d4a
GraceWere <gwere2000@gmail.com>
2023-04-11 05:37:02 -0700

    update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,12 @@
  * Return: 1 on success, -1 on failure
  *
  * Description: This function sets the value of a bit at a specified index to 0
- * in the specified number. It takes a pointer to the number that needs to be changed
- * and the index of the bit that should be cleared as parameters. If the index is
- * greater than 63, the function returns -1 to indicate failure; otherwise, it clears
+ * in the specified number.
+ * It takes a pointer to the number that needs to be changed
+ * and the index of the bit that should be cleared as parameters.
+ * If the index is
+ * greater than 63, the function returns -1 to indicate failure;
+ * otherwise, it clears
  * the bit at the specified index and returns 1 to indicate success.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
